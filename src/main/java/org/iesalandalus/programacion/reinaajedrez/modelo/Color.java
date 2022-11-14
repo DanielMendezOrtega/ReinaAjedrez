29e1472bcb789c3f13dccbe24a49e63d5e01acb7
linoa <linoa@192.168.56.1>
2022-11-14 22:17:02 +0100

    Creando clase posicion ,atributos y metodos getter y setter con excepcion IllegalArgumentException

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -1,23 +1,19 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public enum Color {
-	
-	 BLANCO("Blanco"),
-	 NEGRO("Negro");
-	String cadenaMostrar;
-	
-	
 
-	private Color(String colorReina ) {
-		this.cadenaMostrar=colorReina;
-		
+	BLANCO("Blanco"), NEGRO("Negro");
+
+	String cadenaMostrar;
+
+	private Color(String colorReina) {
+		this.cadenaMostrar = colorReina;
+
 	}
-	
+
 	@Override
 	public String toString() {
 		return String.format(cadenaMostrar);
 	}
-	
-	
-	
+
 }
---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -0,0 +1,51 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Posicion {
+	private int fila;
+	private char columna;
+
+	
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila < 2) {
+			throw new IllegalArgumentException("El numero de la fila no puede ser menor que 1");
+		}else if (fila > 8) {
+			throw new IllegalArgumentException("El numero de fila no puede ser superior a 8");
+		}
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if (columna > 'h') {
+			throw new IllegalArgumentException("Te sales del Tablero; valores de columna -> a,b,c,d,e,f,g,h");
+		}
+		this.columna = columna;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
